9794ba079d33e3081628d969f70064a398943bc8
Pilarmontenegr <106849893+Pilarmontenegr@users.noreply.github.com>
2023-03-31 10:18:14 -0300

    Add files via upload

---
+++ Pilar/ChatGPT/ChatGPT-TURBO/ChatGPT-TURBO/Services/ChatGPT.cs
@@ -0,0 +1,44 @@
+﻿using OpenAI_API.Chat;
+using OpenAI_API.Completions;
+using OpenAI_API.Models;
+using OpenAI_API;
+
+namespace ChatGPT_TURBO.Services
+{
+    
+    public class ChatGPT : IAnswerGeneratorService
+    {
+        
+        public async Task<String> CreateChatCompletionAsync(string prompt)
+        {
+            string apiKey = "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";
+
+            var openai = new OpenAIAPI(apiKey);
+
+
+            var result = await openai.Chat.CreateChatCompletionAsync(new ChatRequest()
+            {
+                Model = Model.ChatGPTTurbo,
+                Temperature = 0.1,
+                MaxTokens = 4000,
+                Messages = new ChatMessage[] {
+                new ChatMessage(ChatMessageRole.User, prompt)
+            }
+            });
+
+            string resultB = "";
+
+            if (result != null)
+            {
+                foreach (var item in result.Choices)
+                {
+                    resultB = item.Message.Content;
+                }
+            }
+
+            return resultB;
+        }
+
+        
+    }
+}
---
+++ Pilar/ChatGPT/ChatGPT-TURBO/ChatGPT-TURBO/Services/IAnswerGeneratorService.cs
@@ -0,0 +1,12 @@
+﻿using OpenAI_API.Chat;
+using OpenAI_API.Completions;
+
+namespace ChatGPT_TURBO.Services
+{
+    public interface IAnswerGeneratorService
+    {
+        Task<String> CreateChatCompletionAsync(string pregunta);
+        
+
+    }
+}
