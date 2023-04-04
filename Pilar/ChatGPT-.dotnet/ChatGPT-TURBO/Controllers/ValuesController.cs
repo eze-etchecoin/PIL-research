e5276aa2aad5b9b66b7cef89e6e8518e0283ba44
Pilarmontenegr <106849893+Pilarmontenegr@users.noreply.github.com>
2023-04-04 13:56:51 -0300

    Add files via upload

---
+++ Pilar/ChatGPT-.dotnet/ChatGPT-TURBO/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using ChatGPT_TURBO.Services;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API.Chat;
+
+namespace ChatGPT_TURBO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<String> ChatCompletion(string prompt)
+        {
+            var ChatGPT1 = new ChatGPT();
+
+
+
+            return await ChatGPT1.CreateChatCompletionAsync(prompt);
+
+
+        }
+    }
+}
