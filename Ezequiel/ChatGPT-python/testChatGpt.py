bc8e88db70a23a3ecf6656e34418fe4ec90b906b
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-04-04 15:50:59 -0300

    probando librería de python

--- Ezequiel/ChatGPT-python/testChatGpt.py
+++ Ezequiel/ChatGPT-python/testChatGpt.py
@@ -1,1 +1,17 @@
-print("hola")
+import openai
+
+API_KEY = "....."
+
+openai.api_key
+
+print("GPT-3.5-turbo Test")
+
+while(True):
+    
+    print("Ask for something...\n")
+    prompt = input()
+
+    if prompt.lower() == "exit":
+        break
+
+print("Adiós...")
